dba6e1d6ddcebea5468508a32c3a55b4f6402a83
juniordevelopersbang <56787096+juniordevelopersbang@users.noreply.github.com>
2020-07-10 23:28:16 +0900

    add proxy

--- server/models/User.js
+++ server/models/User.js
@@ -35,7 +35,7 @@ const userSchema = mongoose.Schema({
 });
 
 userSchema.pre("save", function (next) {
-  var user = this;
+  let user = this;
   if (user.isModified("password")) {
     //비밀번호를 암호화 시킨다.
     bcrypt.genSalt(saltRounds, function (err, salt) {
@@ -61,11 +61,11 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
 };
 
 userSchema.methods.generateToken = function (cb) {
-  var user = this;
+  let user = this;
   // console.log('user._id', user._id)
 
   // jsonwebtoken을 이용해서 token을 생성하기
-  var token = jwt.sign(user._id.toHexString(), "secretToken");
+  let token = jwt.sign(user._id.toHexString(), "secretToken");
   // user._id + 'secretToken' = token
   // ->
   // 'secretToken' -> user._id
@@ -78,7 +78,7 @@ userSchema.methods.generateToken = function (cb) {
 };
 
 userSchema.statics.findByToken = function (token, cb) {
-  var user = this;
+  let user = this;
   // user._id + ''  = token
   //토큰을 decode 한다.
   jwt.verify(token, "secretToken", function (err, decoded) {
